1d92e563d745baf9092b15bc4de72f95403e0006
RJ Routh <94662443+gosukel@users.noreply.github.com>
2025-07-23 20:47:40 -0400

    add fake message database

---
+++ models/fakeDatabase.js
@@ -0,0 +1,42 @@
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+    {
+        text: "Welcome to the world of POKeMON!",
+        user: "Oak",
+        added: new Date(),
+    },
+    {
+        text: "You're a wizard, Harry!",
+        user: "Hagrid",
+        added: new Date(),
+    },
+    {
+        text: "A wizard is never late...",
+        user: "Gandalf",
+        added: new Date(),
+    },
+    {
+        text: "I know...",
+        user: "Han",
+        added: new Date(),
+    },
+    {
+        text: "Make it so!",
+        user: "Jean-Luc",
+        added: new Date(),
+    },
+    {
+        text: "The best way out is always through.",
+        user: "Robert",
+        added: new Date(),
+    },
+];
