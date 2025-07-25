8f61b0f90d0512e2c13aa25d84fbf2c7106a361d
RJ Routh <94662443+gosukel@users.noreply.github.com>
2025-07-24 23:03:09 -0400

    add get, post handlers for 'new' route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 import { Router } from "express";
-import messages from "../models/fakeDatabase.js";
+import messages, { colors } from "../models/fakeDatabase.js";
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
     res.render("index", { messages: messages });
+});
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+    let { username, message } = req.body;
+    let firstChar = username[0].toUpperCase();
+    let newUsername = firstChar + username.slice(1);
+    const newMessage = {
+        text: message,
+        user: newUsername,
+        added: new Date(),
+        color: colors[Math.floor(Math.random() * colors.length)],
+    };
+    messages.push(newMessage);
+    console.log(username);
+    console.log(message);
+    res.redirect("/");
 });
 
 export default indexRouter;
